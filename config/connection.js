53ca14b2eff231996d918d2c6b97ef349e8c7c50
100jobin <114425368+100jobin@users.noreply.github.com>
2024-12-21 21:50:50 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,20 @@ const MongoClient = require('mongodb').MongoClient;
 const state = {
     db: null
 };
+//mongodb+srv://Jobin:<db_password>@cluster0.efxy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//bIOq7XvrlmdTyR3G
 
 module.exports.connect = async function (done) {
-    const url = 'mongodb://127.0.0.1:27017';
+    const url = 'mongodb+srv://Jobin:bIOq7XvrlmdTyR3G@cluster0.efxy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     const dbName = 'shopping';
 
     try {
-        const client = await MongoClient.connect(url);
+        const client = await MongoClient.connect(url,{ useUnifiedTopology: true });
         state.db = client.db(dbName);
         done();
     } catch (err) {
         done(err);
     }
-};
-
-module.exports.get = function () {
-    return state.db;
 };
 
 
