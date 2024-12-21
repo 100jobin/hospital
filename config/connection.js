dd6dd32580c64aeee3334b30a3b2c555a802bd6f
100jobin <114425368+100jobin@users.noreply.github.com>
2024-12-21 21:57:23 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 const state = {
     db: null
 };
-//mongodb+srv://Jobin:<db_password>@cluster0.efxy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-//bIOq7XvrlmdTyR3G
 
 module.exports.connect = async function (done) {
-    const url = 'mongodb+srv://Jobin:bIOq7XvrlmdTyR3G@cluster0.efxy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    const url = 'mongodb+srv://Jobin:bIOq7XvrlmdTyR3G@cluster0.efxy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
     const dbName = 'shopping';
 
     try {
-        const client = await MongoClient.connect(url,{ useUnifiedTopology: true });
-        state.db = client.db(dbName);
-        done();
+        // Connect to the MongoDB cluster
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        state.db = client.db(dbName); // Assign the database instance to state
+        console.log('Database connected successfully');
+        if (done) done(); // Callback after successful connection
     } catch (err) {
-        done(err);
+        console.error('Database connection error:', err);
+        if (done) done(err); // Callback with error
     }
 };
+
+module.exports.get = function () {
+    if (!state.db) {
+        throw new Error('Database not initialized. Call connect() first.');
+    }
+    return state.db;
+};
+
 
 
